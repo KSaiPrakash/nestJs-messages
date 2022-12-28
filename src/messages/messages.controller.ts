181f2bd5705d15eca534dc8691b3d9fdbbc016e8
KSaiPrakash <maverick16.sai@gmail.com>
2022-12-28 13:35:34 +0530

    messages_initial commit

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,32 @@
+import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
+import { CreateMessageDTO } from './dto/create-message.dto';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+
+    constructor(public messagesService: MessagesService) {
+
+    }
+    
+    @Get()
+    listMessages() {
+        return this.messagesService.findAll();
+    }
+
+    @Post()
+    createMessages(@Body() body: CreateMessageDTO) {
+        return this.messagesService.create(body.content);
+    }
+
+    @Get('/:id')
+    async getMessage(@Param('id') id: string) {
+        const message = await this.messagesService.findOne(id);
+
+        if(!message) {
+            throw new NotFoundException('message not found')
+        }
+
+        return message;
+    }
+}
